d980579002629c435861ae91db748c3708f25cd5
iliakaratist1 <dimonkioroglo@gmail.com>
2021-05-17 00:39:00 +0300

    lab2_punkt3.4

--- lab2/module.js
+++ lab2/module.js
@@ -37,3 +37,17 @@ class OnlineCinema extends Cartoons{
         return this.price
     }
 }
+const Begin  = new Films("thriller", "2010", "LA");
+const Lost = new Films("thriller", "2000", "Russia");
+const Batman = new Films("thriller", "2004", "USA");
+const Hobbit = new Films("thriller", "2012", "USA");
+const b1408 = new Films("FilmJanre", "2006", "USA");
+const Kitchen = new Films("thriller", "2004", "CNDR");
+const Godzilla = new Films("FilmJanre", "2016", "Moldova");
+const Love = new Films("thriller", "2010", "India");
+const Noise = new Films("FilmJanre", "2002", "USA");
+const i21Brige = new Films("thriller", "2009", "Somali");
+
+let arr = new Array();
+let arr = [Begin, Lost, Batman, Hobbit, b1408, Kitchen, Godzilla, Love, Noise, i21Bridge]
+ arr.sort([year]);
